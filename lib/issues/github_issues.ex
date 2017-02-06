813bfb63d7fa879ef3f1d00ba745f9f2e3d2464a
Zhang Yi <zhangyi@bigeyedata.com>
2017-02-06 16:41:19 +0800

    use config

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -2,6 +2,7 @@ defmodule Issues.GithubIssues do
   @moduledoc false
 
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -10,10 +11,16 @@ defmodule Issues.GithubIssues do
   end
 
   defp issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  defp handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}), do: {:ok, body}
-  defp handle_response({:ok, %HTTPoison.Response{status_code: 404}}), do: {:error, "Not found"}
-  defp handle_response({:error, %HTTPoison.Error{reason: reason}}), do: {:error, reason}
+  defp handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, :jsx.decode(body)}
+  end
+  defp handle_response({:ok, %HTTPoison.Response{status_code: 404}}) do
+    {:error, "Not found"}
+  end
+  defp handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    {:error, :jsx.decode(reason)}
+  end
 end
